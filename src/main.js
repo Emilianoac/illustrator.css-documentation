57d6ad2921dd3765cd6da76e11a88a0df5a8a182
Emi <47641194+Emilianoac@users.noreply.github.com>
2021-07-07 11:35:11 -0400

    Add illustrator.css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Illustrator from '../node_modules/illustrator.css/dist/css/illustrator.css'
+import Illustrator from './css/illustrator.css'
 import Main from './scss/main.scss'
 
 createApp(App).use(router).use(router).mount('#app')
